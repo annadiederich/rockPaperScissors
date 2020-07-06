b09d7fc572836c9237484e8e56323c3780654977
Anna Diederich <annadiederich@Annas-MacBook-Pro.local>
2020-07-06 12:09:46 -0500

    Third Commit

--- rockPaperScissors/SecondViewController.swift
+++ rockPaperScissors/SecondViewController.swift
@@ -13,8 +13,21 @@ class SecondViewController: UIViewController {
     var userChoice = 0
     var computerChoice = 0
 
+    @IBOutlet weak var computerPickedImage: UIImageView!
+    @IBOutlet weak var userPickedImage: UIImageView!
+    
+    @IBOutlet weak var whoWon: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        generateComputerChoice()
+        compare()
+        populateImages()
+        
+        userPickedImage.image = UIImage(named: "rock")
+        
 
         // Do any additional setup after loading the view.
     }
@@ -23,19 +36,43 @@ class SecondViewController: UIViewController {
         computerChoice = number
     }
     
+    func populateImages () {
+        if userChoice == 0 {
+            userPickedImage.image = UIImage(named: "rock")
+        } else if userChoice == 1 {
+            userPickedImage.image = UIImage(named: "scissors")
+        } else if userChoice == 2 {
+            userPickedImage.image = UIImage(named: "paper")
+        }
+        if computerChoice == 0 {
+            computerPickedImage.image = UIImage(named: "rock")
+        } else if computerChoice == 1 {
+            computerPickedImage.image = UIImage(named: "scissors")
+        } else if computerChoice == 2 {
+            computerPickedImage.image = UIImage(named: "paper")
+        }
+    }
+    
+    
     func compare () {
         if computerChoice == userChoice {
             print ("It's a tie")
+            
+            whoWon.text = "It's a tie"
         } else if (userChoice == 0 && computerChoice == 1) {
             print ("You won")
+             whoWon.text = "You won"
             
         } else if (userChoice == 1 && computerChoice == 2) {
             print ("You won")
+             whoWon.text = "You won"
             
         } else if (userChoice == 2 && computerChoice == 0) {
             print ("You won")
+             whoWon.text = "You won"
         } else {
-            print ("You lost") 
+            print ("You lost")
+             whoWon.text = "You lost"
             
         }
         
